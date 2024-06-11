09d1ab370100fd9bd8396735d95c081deff23e70
ankita239 <ankitaacharya.aa.01@gmail.com>
2024-06-11 14:41:57 +0530

    fourth commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -77,7 +77,6 @@ const handleremovextra = () => {
           <button disabled={text.length===0} className="bg-blue-800 rounded text-white font-semibold p-2 m-1" onClick={handleUp}>UpperCase</button>
           <button disabled={text.length===0} className="bg-blue-800 rounded text-white font-semibold p-2 m-1" onClick={handleLow}>LowerCase</button>
           <button disabled={text.length===0} className="bg-blue-800 rounded text-white font-semibold p-2 m-1" onClick={handleTitle}>Title Case</button>
-          <button disabled={text.length===0} className="bg-blue-800 rounded text-white font-semibold p-2 m-1" onClick={handleSentence}>Sentence Case</button>
           <button disabled={text.length===0} type="submit" onClick={speak} className="bg-blue-800 rounded text-white font-semibold p-2 m-1">Speak</button>
           <button disabled={text.length===0} type="button" className="bg-blue-800 rounded text-white font-semibold p-2 m-1" onClick={handlestopclicked}>Stop Listening </button>
           <button disabled={text.length===0} type="button" className="bg-blue-800 rounded text-white font-semibold p-2 m-1" onClick={handleremovextra}>Remove Extra Spaces </button>
